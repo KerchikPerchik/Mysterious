122965ae248213cce87fdb1edc61a290c7b422d0
KerchikPerchik <148487415+KerchikPerchik@users.noreply.github.com>
2023-10-22 00:29:26 +0300

    Add files via upload

---
+++ folder/client_packages/cs_packages/Client/Main.cs
@@ -0,0 +1,14 @@
+﻿using RAGE;
+using RAGE.Game;
+
+public class Main : Events.Script
+{
+    public Main()
+    {
+        Events.OnPlayerReady += OnPlayerReady;
+    }
+    public void OnPlayerReady()
+    {
+        Chat.Output("Добро пожаловать!");
+    }
+}
