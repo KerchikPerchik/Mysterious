4263dff2b21d3ac1f9fe934af52d509455cb7ea6
KerchikPerchik <148487415+KerchikPerchik@users.noreply.github.com>
2023-10-22 00:27:44 +0300

    Add files via upload

---
+++ folder/Server/Server/Events.cs
@@ -0,0 +1,19 @@
+﻿using GTANetworkAPI;
+using MySql.Data.MySqlClient;
+
+public class Events : Script
+{
+    [ServerEvent(Event.ResourceStart)]
+    public void OnResourceStart()
+    {
+        NAPI.Util.ConsoleOutput("Hello world");
+        MySQL.Test();
+
+        string insertQuery = "INSERT INTO users.address (idaddress) (address) VALUES (@idaddress) (@address)";
+        using MySqlCommand insertCommand = new MySqlCommand(insertQuery);
+        insertCommand.Parameters.AddWithValue("@idaddress", 1);
+        insertCommand.Parameters.AddWithValue("@address", "Kirill_Mukhachev");
+
+        MySQL.Query(insertCommand);
+    }
+}
---
+++ folder/Server/Server/MySQL/MySQL.cs
@@ -0,0 +1,75 @@
+﻿using GTANetworkAPI;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public class MySQL
+{
+    private static readonly string connStr = "server=localhost;user=root;database=MySQL;password=1234;Pooling=true;";
+
+    public static void Test()
+    {
+        using MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            conn.Open();
+            conn.Close();
+            NAPI.Util.ConsoleOutput("Подключение к MySQL успешно");
+        }
+        catch (Exception ex)
+        {
+            NAPI.Util.ConsoleOutput(ex.ToString());
+            NAPI.Util.ConsoleOutput("Подключение к MySQL не успешно");
+        }
+    }
+
+    public static void Query(MySqlCommand command)
+    {
+        if (command == null || command.CommandText.Length < 1)
+        {
+            NAPI.Util.ConsoleOutput("Wrong command arg: null or empty");
+            return;
+        }
+        using MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            conn.Open();
+            command.Connection = conn;
+            command.ExecuteNonQuery();
+            conn.Close();
+
+        }
+        catch (Exception ex)
+        {
+            NAPI.Util.ConsoleOutput(ex.ToString());
+        }
+    }
+
+    public static DataTable QueryRead(MySqlCommand command)
+    {
+        if (command == null || command.CommandText.Length < 1)
+        {
+            NAPI.Util.ConsoleOutput("Wrong command arg: null or empty");
+            return null;
+        }
+        using MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            conn.Open();
+            command.Connection = conn;
+
+            using MySqlDataReader reader = command.ExecuteReader();
+            using (DataTable dt = new DataTable())
+            {
+                dt.Load(reader);
+                return dt;
+            }
+        }
+        catch (Exception ex)
+        {
+            NAPI.Util.ConsoleOutput(ex.ToString());
+            return null;
+        }
+    }
+}
